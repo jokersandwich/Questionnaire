91e744388a3a17663e06ff0dbd11a8133084778b
jokersandwich <410111643@qq.com>
2017-10-01 16:19:37 +0800

    Edit问题样式

--- src/components/Edit/Edit.jsx
+++ src/components/Edit/Edit.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DatePicker, Button, Input } from 'antd';
+import { DatePicker, Button, Input, Radio, Checkbox } from 'antd';
 import style from './Edit.css';
 
 class Edit extends React.Component {
@@ -42,13 +42,91 @@ class Edit extends React.Component {
         return (
             this.state.titleEditable ? (
                 <div className="editTitle" style={{ margin: 20, padding: 20, textAlign: 'center' }} onClick={this.handleTitleClick}>
-                    <Input style={{ fontSize: 18, fontWeight: 'bold', padding: 30, textAlign: 'center' }} value={this.state.title} onChange={this.handleTitleChange} onBlur={this.handleTitleBlur} />
+                    <Input 
+                        style={{ fontSize: 18, fontWeight: 'bold', padding: 30, textAlign: 'center' }} 
+                        ref="input"
+                        value={this.state.title} 
+                        onChange={this.handleTitleChange} 
+                        onBlur={this.handleTitleBlur} 
+                    />
                 </div>
             ) : (
                 <div className="editTitle" style={{ margin: 20, padding: 20, textAlign: 'center' }} onClick={this.handleTitleClick}>
                     <h2><strong>{this.state.title}</strong></h2>
                 </div>
             )
+        );
+    }
+
+    getQuestions() {
+        const { TextArea } = Input;
+        const radioStyle = {
+            display: 'block',
+            height: '28px',
+            lineHeight: '28px',
+            marginLeft: '32px'
+        };
+        return (
+            <div style={{ marginBottom: 20 }}>
+                <div className="questionsWrap" style={{ padding: 30 }}>
+                    <p>
+                        <span style={{ position: 'absolute' }}>Q1</span>
+                        <span style={{ marginLeft: 32 }}>单选题</span>
+                    </p>
+                    <Radio.Group>
+                        <Radio style={radioStyle} value={1}>A</Radio>
+                        <Radio style={radioStyle} value={2}>B</Radio>
+                        <Radio style={radioStyle} value={3}>C</Radio>
+                        <Radio style={radioStyle} value={4}>D</Radio>
+                    </Radio.Group>
+                    <div>
+                        <p style={{ float: 'right' }}>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>上移</span>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>下移</span>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>复用</span>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>删除</span>
+                        </p>
+                    </div>
+                </div>
+                <div className="questionsWrap" style={{ padding: 30 }}>
+                    <p>
+                        <span style={{position: 'absolute' }}>Q2</span>
+                        <span style={{ marginLeft: 32 }}>多选题</span>
+                    </p>
+                    <Checkbox.Group>
+                        <Checkbox style={radioStyle} value={1}>A</Checkbox>
+                        <Checkbox style={radioStyle} value={2}>B</Checkbox>
+                        <Checkbox style={radioStyle} value={3}>C</Checkbox>
+                        <Checkbox style={radioStyle} value={4}>D</Checkbox>
+                    </Checkbox.Group>
+                    <div>
+                        <p style={{ float: 'right' }}>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>上移</span>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>下移</span>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>复用</span>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>删除</span>
+                        </p>
+                    </div>
+                </div>
+                <div className="questionsWrap" style={{ padding: 30 }}>
+                    <p>
+                        <span style={{ position: 'absolute' }}>Q3</span>
+                        <span style={{ marginLeft: 32 }}>文本题</span>
+                    </p>
+                    <div style={{ margin: '8px 32px' }}>
+                        <TextArea rows={5} cols={10}/>
+                    </div>
+                    <Checkbox style={radioStyle} value={false}>此题是否必填</Checkbox>
+                    <div>
+                        <p style={{ float: 'right' }}>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>上移</span>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>下移</span>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>复用</span>
+                            <span className="questionsOperate" style={{ marginLeft: 8 }}>删除</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
         );
     }
 
@@ -70,8 +148,6 @@ class Edit extends React.Component {
         );
     }
 
-    getQuestionArea() {}
-
     handleDatePick(date, dateString) {
         this.setState({
             date: dateString
@@ -83,8 +159,9 @@ class Edit extends React.Component {
             <div>
                 {this.getTitle()}
                 <div style={{ padding: 20, borderTop: '2px solid #ccc', borderBottom: '2px solid #ccc' }}>
+                    {this.getQuestions()}
                     {this.getAddArea()}
-                    <div className="eidtAddQuestion" style={{ wdith: '100%', height: '100%', padding: 30, background: '#eee', textAlign: 'center'}} onClick={this.handleAddQuestion}>
+                    <div className="addQuestion" style={{ wdith: '100%', height: '100%', padding: 30, background: '#eee', textAlign: 'center'}} onClick={this.handleAddQuestion}>
                         添加问题
                     </div>
                 </div>
